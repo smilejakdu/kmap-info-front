479f1bb24720b77530ee3a3eaabb53445cab4c80
smilejakdu <ash982416@gmail.com>
2020-06-30 17:25:05 +0900

    compound_page_body2

--- src/components/Compound/CompoundInfo.js
+++ src/components/Compound/CompoundInfo.js
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from "react";
 import "./CompoundInfo.scss";
 import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
+import { IoIosRadioButtonOff } from "react-icons/io";
 import image from "./test.png";
 const CompoundInfo = () => {
   return (
@@ -10,7 +11,7 @@ const CompoundInfo = () => {
       <div className="compound_info_header">
         {/* KaiChem ID  */}
         <div className="kaichem_id_border">
-          <table className="table-size">
+          <table>
             <tr>
               <th className="kaichem_id">KaiChem ID</th>
               <td className="kaichem_id_value">KC0006</td>
@@ -37,8 +38,78 @@ const CompoundInfo = () => {
         Acebutolol hydrochloride
       </div>
       <div className="compound_info_body">
-        <img src={image} className="pubchem_image_left" />
-        <div className="pubchem_image_right">sdf</div>
+        {/* left start */}
+        <img src={image} className="pubchem_body_left" />
+        {/* right start */}
+        <div className="pubchem_body_right">
+          <div className="pubchem_cid_table_border">
+            <table className="table-bordered">
+              <tr className="pubchem_cid_tr_border">
+                <th className="pubchem_cid">pubchem CID</th>
+                <td className="pubchem_cid_value">441307</td>
+                <th className="pubchem_cid">InChIKey</th>
+                <td className="inchikey_value">KDSFLKJDLFKJSDFk</td>
+              </tr>
+              <tr>
+                <td className="pubchem_cid">PubChem NAME</td>
+                <td colSpan="3" className="pubchem_cid_value">
+                  Acebutolol hydrochioride
+                </td>
+              </tr>
+            </table>
+          </div>
+          <table className="table-bordered kmap-2k-subset-border">
+            <tr>
+              <td className="kmap-2k-subset">KMAP-2K Subset</td>
+              <td className="kmap-2k-subset-value">P09</td>
+            </tr>
+          </table>
+          <table className="ipk_border">
+            <tr>
+              <td className="ipk-library">
+                <IoIosRadioButtonOff />
+              </td>
+              <td>IPK Expansion</td>
+            </tr>
+            <tr>
+              <td>
+                <IoIosRadioButtonOff />
+              </td>
+              <td>Prestwick</td>
+            </tr>
+            <tr>
+              <td>
+                <IoIosRadioButtonOff />
+              </td>
+              <td>SelleckChem</td>
+            </tr>
+          </table>
+          <img
+            src={image}
+            width="200px"
+            height="200px"
+            className="pubchem-right-image"
+          />
+          <div>
+            <table>
+              <tr>
+                <td>
+                  <input type="checkbox" />
+                </td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td></td>
+              </tr>
+            </table>
+          </div>
+        </div>
+        {/* right end */}
       </div>
     </>
   );
