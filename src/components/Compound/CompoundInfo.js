8c88dbae34384384fca9e1dcee48745f07719168
smilejakdu <ash982416@gmail.com>
2020-07-06 20:30:17 +0900

    compound_page_modify

--- src/components/Compound/CompoundInfo.js
+++ src/components/Compound/CompoundInfo.js
@@ -1,106 +1,21 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-/* eslint-disable jsx-a11y/alt-text */
 import React, { useState, useEffect } from "react";
 import "./CompoundInfo.scss";
 import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
 import request from "../../util/request";
 
-const CompoundInfo = (search_data) => {
-  console.log("CompoundInfo :", search_data);
-  console.log("CompoundInfo :", search_data.data);
-  console.log("CompoundInfo :", search_data.data[0]);
-  const [chemindex, setChemindex] = useState(1);
-  const [compoundinfo, setCompoundinfo] = useState([]);
-
-  const previousBtn = () => {
-    if (chemindex === 1) {
-      return;
-    }
-    setChemindex(chemindex - 1);
-  };
-
-  const nextBtn = () => {
-    setChemindex(chemindex + 1);
-  };
-
-  useEffect(() => {
-    request
-      .get(`/compound/${chemindex}`)
-      .then((res) => {
-        let {
-          data: { data },
-        } = res;
-        if (data.length === 0) {
-          alert("데이터가 없습니다.");
-          previousBtn();
-        }
-        console.log(data[0]);
-        setCompoundinfo({
-          chem_series: data[0].chem_series,
-          chem_series_cid: data[0].chem_series_cid,
-          cid: data[0].cid,
-          compound: data[0].compound,
-          europe: data[0].europe,
-          inchikey: data[0].inchikey,
-          information: data[0].information,
-          ipk: data[0].ipk,
-          japan: data[0].japan,
-          kaichem_id: data[0].kaichem_id,
-          kaipharm_chem_index: data[0].kaipharm_chem_index,
-          known_target: data[0].known_target,
-          nci_cancer: data[0].nci_cancer,
-          prestwick: data[0].prestwick,
-          pubchem_name: data[0].pubchem_name,
-          selleckchem: data[0].selleckchem,
-          subset: data[0].subset,
-          usa: data[0].usa,
-        });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, [chemindex]);
+const CompoundInfo = (compoundinfo) => {
+  console.log("CompoundInfo.js :", compoundinfo.data);
+  console.log("CompoundInfo.js :", compoundinfo.data.compound);
 
   return (
     <>
-      <h1 className="test_title">Compound page.</h1>
-      <div className="compound_info_header">
-        {/* KaiChem ID  */}
-        <div className="kaichem_id_border">
-          <table>
-            <tr>
-              <th className="kaichem_id">KaiChem ID</th>
-              <td className="kaichem_id_value">{compoundinfo["kaichem_id"]}</td>
-            </tr>
-          </table>
-        </div>
-        {/* KaiPHarm Chem Index */}
-        <div className="kaipharm_chem_index_div">
-          <div className="kaipharm_chem_index_button">
-            <BsChevronCompactLeft onClick={previousBtn}>
-              왼쪽
-            </BsChevronCompactLeft>
-          </div>
-          <div className="kaipharm_chem_index">
-            <div className="kaipharm_chem_index_header">
-              KaiPharm Chem Index
-            </div>
-            <div className="kaipharm_chem_index_body">{chemindex}</div>
-          </div>
-          <div className="kaipharm_chem_index_button">
-            <BsChevronCompactRight onClick={nextBtn}>
-              오른쪽
-            </BsChevronCompactRight>
-          </div>
-        </div>
-      </div>
       <div className="compound_info_hydrochloride">
-        {compoundinfo["compound"]}
+        {compoundinfo.data.compound}
       </div>
       <div className="compound_info_body">
         {/* left start */}
         <img
-          src={`https://pubchem.ncbi.nlm.nih.gov/image/imgsrv.fcgi?cid=${compoundinfo["cid"]}&t=l`}
+          src={`https://pubchem.ncbi.nlm.nih.gov/image/imgsrv.fcgi?cid=${compoundinfo.data.cid}&t=l`}
           className="pubchem_body_left"
         />
         {/* right start */}
@@ -110,14 +25,16 @@ const CompoundInfo = (search_data) => {
               <table className="table-bordered">
                 <tr className="pubchem_cid_tr_border">
                   <td className="pubchem_cid">pubchem CID</td>
-                  <td className="pubchem_cid_value">{compoundinfo["cid"]}</td>
+                  <td className="pubchem_cid_value">{compoundinfo.data.cid}</td>
                   <td className="pubchem_cid">InChIKey</td>
-                  <td className="inchikey_value">{compoundinfo["inchikey"]}</td>
+                  <td className="inchikey_value">
+                    {compoundinfo.data.inchikey}
+                  </td>
                 </tr>
                 <tr>
                   <td className="pubchem_cid">PubChem NAME</td>
                   <td colSpan="3" className="pubchem_cid_value">
-                    {compoundinfo["pubchem_name"]}
+                    {compoundinfo.data.pubchem_name}
                   </td>
                 </tr>
               </table>
@@ -128,14 +45,14 @@ const CompoundInfo = (search_data) => {
               <tr>
                 <td className="kmap-2k-subset">KMAP-2K Subset</td>
                 <td className="kmap-2k-subset-value">
-                  {compoundinfo["subset"]}
+                  {compoundinfo.data.subset}
                 </td>
               </tr>
             </table>
             <table className="ipk_border">
               <tr>
                 <td>
-                  {compoundinfo["ipk"] === 1 ? (
+                  {compoundinfo.data.ipk === 1 ? (
                     <span className="dot_active" />
                   ) : (
                     <span className="dot" />
@@ -145,7 +62,7 @@ const CompoundInfo = (search_data) => {
               </tr>
               <tr>
                 <td>
-                  {compoundinfo["prestwick"] === 1 ? (
+                  {compoundinfo.data.prestwick === 1 ? (
                     <span className="dot_active" />
                   ) : (
                     <span className="dot" />
@@ -155,7 +72,7 @@ const CompoundInfo = (search_data) => {
               </tr>
               <tr>
                 <td>
-                  {compoundinfo["selleckchem"] === 1 ? (
+                  {compoundinfo.data.selleckchem === 1 ? (
                     <span className="dot_active" />
                   ) : (
                     <span className="dot" />
@@ -168,7 +85,7 @@ const CompoundInfo = (search_data) => {
               <table>
                 <tr>
                   <td>
-                    {compoundinfo["usa"] === 1 ? (
+                    {compoundinfo.data.usa === 1 ? (
                       <input type="checkbox" checked="checked" />
                     ) : (
                       <input type="checkbox" />
@@ -178,7 +95,7 @@ const CompoundInfo = (search_data) => {
                 </tr>
                 <tr>
                   <td>
-                    {compoundinfo["europe"] === 1 ? (
+                    {compoundinfo.data.europe === 1 ? (
                       <input type="checkbox" checked="checked" />
                     ) : (
                       <input type="checkbox" />
@@ -188,7 +105,7 @@ const CompoundInfo = (search_data) => {
                 </tr>
                 <tr>
                   <td>
-                    {compoundinfo["japan"] === 1 ? (
+                    {compoundinfo.data.japan === 1 ? (
                       <input type="checkbox" checked="checked" />
                     ) : (
                       <input type="checkbox" />
@@ -198,7 +115,7 @@ const CompoundInfo = (search_data) => {
                 </tr>
                 <tr>
                   <td>
-                    {compoundinfo["nci_cancer"] === 1 ? (
+                    {compoundinfo.data.nci_cancer === 1 ? (
                       <input type="checkbox" checked="checked" />
                     ) : (
                       <input type="checkbox" />
@@ -209,7 +126,7 @@ const CompoundInfo = (search_data) => {
               </table>
             </div>
             <img
-              src={`https://pubchem.ncbi.nlm.nih.gov/image/imgsrv.fcgi?cid=${compoundinfo["chem_series_cid"]}&t=l`}
+              src={`https://pubchem.ncbi.nlm.nih.gov/image/imgsrv.fcgi?cid=${compoundinfo.data.chem_series_cid}&t=l`}
               className="pubchem-right-image"
             />
           </div>
@@ -221,7 +138,7 @@ const CompoundInfo = (search_data) => {
                     Known Target
                   </td>
                   <td className="right_body_footer_header_target" rowSpan="2">
-                    {compoundinfo["known_target"]}
+                    {compoundinfo.data.known_target}
                   </td>
                   <td className="right_body_footer_header_gray_border_chem">
                     Chem Group
@@ -231,8 +148,8 @@ const CompoundInfo = (search_data) => {
                   </td>
                 </tr>
                 <tr className="pubchem_cid_tr_border">
-                  <td>{compoundinfo["chem_series"]}</td>
-                  <td>{compoundinfo["chem_series_cid"]}</td>
+                  <td>{compoundinfo.data.chem_series}</td>
+                  <td>{compoundinfo.data.chem_series_cid}</td>
                 </tr>
               </table>
               <div></div>
@@ -240,9 +157,9 @@ const CompoundInfo = (search_data) => {
                 <tr>
                   <td className="information">information</td>
                   <td>
-                    {compoundinfo["information"] ? (
+                    {compoundinfo.data.information ? (
                       <textarea name="" cols="150" rows="4">
-                        {compoundinfo["information"]}
+                        {compoundinfo.data.information}
                       </textarea>
                     ) : (
                       <textarea name="" cols="150" rows="4">
--- src/pages/CompoundInfoPage/CompoundInfoPage.js
+++ src/pages/CompoundInfoPage/CompoundInfoPage.js
@@ -1,41 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./CompoundInfoPage.scss";
 import Navigation from "../../components/Navigation/Navigation";
 import CompoundInfo from "../../components/Compound/CompoundInfo";
 import image from "./logo.png";
 import request from "../../util/request";
 import axios from "axios";
+import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
 
-class CompoundInfoPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      query: "",
-      result: [],
-      compoundname: [],
-    };
-    this.cancel = "";
-  }
+const CompoundInfoPage = () => {
+  const [query, setQuery] = useState("");
+  const [result, setResult] = useState([]);
+  const [chemindex, setChemindex] = useState(1);
+  const [compoundinfo, setCompoundinfo] = useState([]);
 
-  fetchSearchResults = (query) => {
+  const fetchSearchResults = (query) => {
     const searchUrl = `/compound/search?query=${query}`;
-
-    if (this.cancel) {
-      this.cancel.cancel();
-    }
-    this.cancel = axios.CancelToken.source();
     request
-      .get(searchUrl, {
-        cancelToken: this.cancel.token,
-      })
+      .get(searchUrl)
       .then((res) => {
         let {
           data: { data },
         } = res;
         console.log("fetch : ", data);
-        this.setState({
-          result: data,
-        });
+        setResult(data);
       })
       .catch((error) => {
         if (axios.isCancel(error) || error) {
@@ -44,7 +31,17 @@ class CompoundInfoPage extends React.Component {
       });
   };
 
-  searchDataClick = (search_data) => {
+  const previousBtn = () => {
+    if (chemindex === 1) {
+      return;
+    }
+    setChemindex(chemindex - 1);
+  };
+  const nextBtn = () => {
+    setChemindex(chemindex + 1);
+  };
+
+  const searchDataClick = (search_data) => {
     console.log("searchDataClick 들어옴 : ", search_data);
     return request
       .get(`/compound/search/${search_data}`)
@@ -52,28 +49,44 @@ class CompoundInfoPage extends React.Component {
         let {
           data: { data },
         } = res;
-        console.log(data);
-        this.setState({
-          compoundname: data,
+        console.log("searchDataClick : ", data);
+        setCompoundinfo({
+          id: data[0].id,
+          chem_series: data[0].chem_series,
+          chem_series_cid: data[0].chem_series_cid,
+          cid: data[0].cid,
+          compound: data[0].compound,
+          europe: data[0].europe,
+          inchikey: data[0].inchikey,
+          information: data[0].information,
+          ipk: data[0].ipk,
+          japan: data[0].japan,
+          kaichem_id: data[0].kaichem_id,
+          kaipharm_chem_index: data[0].kaipharm_chem_index,
+          known_target: data[0].known_target,
+          nci_cancer: data[0].nci_cancer,
+          prestwick: data[0].prestwick,
+          pubchem_name: data[0].pubchem_name,
+          selleckchem: data[0].selleckchem,
+          subset: data[0].subset,
+          usa: data[0].usa,
         });
+        setResult([]);
       })
       .catch((error) => {
         error && console.warn(error);
       });
   };
 
-  renderSearchResults = () => {
-    const { result } = this.state;
-    console.log("renderSearchResults :", result);
+  const renderSearchResults = () => {
     if (Object.keys(result).length && result.length) {
-      console.log("renderSearchResults 통과 ");
       return (
         <div className="test_div">
           {result.map((res) => {
             return (
               <h6
                 className="search_font"
-                onClick={() => this.searchDataClick(res.compound)}
+                onClick={() => searchDataClick(res.compound)}
               >
                 {res.compound}
               </h6>
@@ -84,48 +97,115 @@ class CompoundInfoPage extends React.Component {
     }
   };
 
-  handleOnInputChange = (event) => {
+  const handleOnInputChange = (event) => {
     const query = event.target.value;
-    console.log("handleOnInputChange : ", query);
     if (!query) {
-      this.setState({ query, result: [] });
+      setQuery(query);
+      setResult([]);
     } else {
-      this.setState({ query }, () => {
-        this.fetchSearchResults(query);
-      });
+      setQuery(query);
+      fetchSearchResults(query);
     }
   };
 
-  render() {
-    const { query } = this.state;
-    console.log("this.state.result", this.state.result);
-    return (
-      <>
-        <div className="nav">
-          <Navigation />
+  useEffect(() => {
+    request
+      .get(`/compound/${chemindex}`)
+      .then((res) => {
+        let {
+          data: { data },
+        } = res;
+        console.log("useEffect : ", data);
+        setCompoundinfo({
+          id: data[0].id,
+          chem_series: data[0].chem_series,
+          chem_series_cid: data[0].chem_series_cid,
+          cid: data[0].cid,
+          compound: data[0].compound,
+          europe: data[0].europe,
+          inchikey: data[0].inchikey,
+          information: data[0].information,
+          ipk: data[0].ipk,
+          japan: data[0].japan,
+          kaichem_id: data[0].kaichem_id,
+          kaipharm_chem_index: data[0].kaipharm_chem_index,
+          known_target: data[0].known_target,
+          nci_cancer: data[0].nci_cancer,
+          prestwick: data[0].prestwick,
+          pubchem_name: data[0].pubchem_name,
+          selleckchem: data[0].selleckchem,
+          subset: data[0].subset,
+          usa: data[0].usa,
+        });
+        setChemindex(data[0].id);
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("데이터가 없습니다.");
+        previousBtn();
+      });
+  }, [chemindex]);
+
+  return (
+    <>
+      <div className="nav">
+        <Navigation />
+      </div>
+      <div>
+        <div className="kai_logo">
+          <img src={"/kmapinfo" + image} width="250" height="100" />
+          {/* <img src={image} width="250" height="100" /> */}
         </div>
+      </div>
+      <div className="div_border">
         <div>
-          <div className="kai_logo">
-            <img src={"/kmapinfo" + image} width="250" height="100" />
-            {/* <img src={image} width="250" height="100" /> */}
+          <input
+            type="text"
+            value={query}
+            placeholder="검색"
+            className="search_input"
+            onChange={handleOnInputChange}
+          />
+        </div>
+        <div>{renderSearchResults()}</div>
+
+        <h1 className="test_title">Compound page.</h1>
+        <div className="compound_info_header">
+          {/* KaiChem ID  */}
+          <div className="kaichem_id_border">
+            <table>
+              <tr>
+                <th className="kaichem_id">KaiChem ID</th>
+                <td className="kaichem_id_value">{compoundinfo.kaichem_id}</td>
+              </tr>
+            </table>
+          </div>
+          {/* KaiPHarm Chem Index */}
+          <div className="kaipharm_chem_index_div">
+            <div className="kaipharm_chem_index_button">
+              <BsChevronCompactLeft onClick={previousBtn}>
+                왼쪽
+              </BsChevronCompactLeft>
+            </div>
+            <div className="kaipharm_chem_index">
+              <div className="kaipharm_chem_index_header">
+                KaiPharm Chem Index
+              </div>
+              <div className="kaipharm_chem_index_body">
+                {compoundinfo.kaipharm_chem_index}
+              </div>
+            </div>
+            <div className="kaipharm_chem_index_button">
+              <BsChevronCompactRight onClick={nextBtn}>
+                오른쪽
+              </BsChevronCompactRight>
+            </div>
           </div>
         </div>
-        <div className="div_border">
-          <div>
-            <input
-              type="text"
-              value={query}
-              placeholder="검색"
-              className="search_input"
-              onChange={this.handleOnInputChange}
-            />
-          </div>
-          <div>{this.renderSearchResults()}</div>
-          <CompoundInfo data={this.state.compoundname} />
-        </div>
-      </>
-    );
-  }
-}
+        <CompoundInfo data={compoundinfo} />
+      </div>
+    </>
+  );
+};
 
 export default CompoundInfoPage;
