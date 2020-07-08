0d2826aa7b47032499032acb8a2aaea6f93eb6dd
smilejakdu <ash982416@gmail.com>
2020-07-08 10:42:15 +0900

    background_path

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -46,7 +46,7 @@ const LoginForm = ({ authenticated, login, location }) => {
     position: "relative",
     width: "100%",
     height: "100%",
-    backgroundImage: `url(${background})`,
+    backgroundImage: `url(/kmapinfo/${background})`,
   };
 
   return (
