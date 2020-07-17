58acf2df2b9a4466499ca2d5ee9aca5cadca3970
smilejakdu <ash982416@gmail.com>
2020-07-17 11:40:51 +0900

    excelinfopage-header-margin-bottom

--- src/pages/ExcelInfo/ExcelInfoPage.style.js
+++ src/pages/ExcelInfo/ExcelInfoPage.style.js
@@ -4,13 +4,13 @@ import palette from "../../util/styles/palette";
 const ExcelInfoPageHeader = styled.div`
   position: relative;
   margin-top: 10rem;
+  margin-bottom: 10rem;
   display: flex;
   margin-right: auto;
   margin-left: auto;
   width: 80%;
 `;
 const ExcelInfoPageBody = styled.div`
-  margin-top: 10rem;
   margin-right: auto;
   margin-left: auto;
   width: 80%;
