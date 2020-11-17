916f4a3873568f59a14a4e6ca5f8b8967846f1df
smilejakdu <ash982416@gmail.com>
2020-11-17 10:51:58 +0900

    front_year_month

--- src/components/Statistics/Column/ColumnChart.js
+++ src/components/Statistics/Column/ColumnChart.js
@@ -1,68 +1,30 @@
-import React, { PureComponent, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./ColumnChart.scss";
-import {
-  BarChart,
-  Bar,
-  Cell,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-} from "recharts";
-import "./ColumnChart.scss";
+import { Bar } from "react-chartjs-2";
 
-const ColumnChart = (data) => {
-  const [widthState, setWidthState] = useState(800);
-  const [heightState, setHeightState] = useState(350);
-  const [marginState, setMarginState] = useState({});
-
-  const updateDimesions = () => {
-    if (window.innerWidth < 2000 && window.innerWidth > 1550) {
-      setWidthState(750);
-      setHeightState(350);
-    } else if (window.innerWidth > 2000 && window.innerWidth < 2200) {
-      setWidthState(850);
-      setHeightState(350);
-    } else if (window.innerWidth < 1550) {
-      setWidthState(950);
-      setHeightState(350);
-    } else if (window.innerWidth > 2200 && window.innerWidth < 2600) {
-      let update_width = window.innerWidth - 1200;
-      let update_height = Math.round(update_width / 3.4);
-      setWidthState(update_width);
-      setHeightState(update_height);
-    } else if (window.innerWidth > 2600) {
-      setWidthState(1300); // 1250
-      setHeightState(350); // 350
-    }
+const ColumnChart = ({ labels, datasets, year_list, YearChangeBtn }) => {
+  console.log("datasets : ", datasets);
+  const options = {
+    responsive: true,
+    legend: {
+      display: false,
+    },
+    type: "bar",
   };
 
-  useEffect(() => {
-    updateDimesions();
-    window.addEventListener("resize", updateDimesions);
-  });
+  const data = {
+    labels,
+    datasets,
+  };
 
   return (
-    <div className="columns_border">
-      <BarChart
-        width={widthState}
-        height={heightState}
-        data={data.data}
-        margin={{
-          top: 5,
-          right: 30,
-          left: 20,
-          bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Bar dataKey="value" fill="#82ca9d" />
-      </BarChart>
+    <div>
+      <div>
+        {year_list.map((year) => (
+          <button onClick={() => YearChangeBtn(year)}> {year}</button>
+        ))}
+      </div>
+      <Bar data={data} width={900} height={500} options={options} />
     </div>
   );
 };
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -9,32 +9,138 @@ import { CircleBorder, ColumnBorder, SvgBorder } from "./Statistics.style";
 const Statistics = () => {
   const [circlepercent, setCirclepercent] = useState(0);
   const [circlechemnum, setCirclechemnum] = useState(0);
-  const [column, setColumn] = useState([]);
+  const [column, setColumn] = useState();
   const [svgdata, setSvgdata] = useState();
+  const [year, setYear] = useState();
+  const [year_list, setYearList] = useState([]);
+  const [labels, setLabels] = useState([]);
+
+  const [datasets, setDatasets] = useState();
 
   useEffect(() => {
     request
       .get("/excel/statistics")
       .then(({ data }) => {
-        console.log(data);
         const {
           circle_number,
           columns_list,
           kaichem_number,
           svg_data_list,
         } = data;
+
         setCirclepercent(circle_number);
         setCirclechemnum(kaichem_number);
         setColumn(columns_list);
         setSvgdata(svg_data_list);
 
-        console.log(columns_list);
-        console.log(columns_list["2020"]);
+        let yearData = [];
+        for (const year in columns_list) {
+          yearData.push(year);
+        }
+        setYearList(yearData);
       })
+
       .catch((err) => {
         err && console.log(err);
       });
   }, []);
+
+  useEffect(() => {
+    const month_list = [];
+    for (const data in column) {
+      // console.log(data, column[`${data}`]);
+      if (year === data) {
+        console.log(data, column[`${data}`]);
+
+        const month_list = [];
+        for (const month in column[`${data}`]) {
+          month_list.push(month);
+        }
+
+        // month sort
+        const number_sort = month_list.sort(function (a, b) {
+          return a - b;
+        });
+        // labels month input
+        const test = [];
+
+        for (const number_month of number_sort) {
+          test.push(`${number_month}month`);
+        }
+        setLabels(test);
+        const one_list = [];
+        const two_list = [];
+        const three_list = [];
+        const four_list = [];
+        const five_list = [];
+        const six_list = [];
+
+        for (const number of number_sort) {
+          one_list.push(column[year][number][0]);
+          two_list.push(column[year][number][1]);
+          three_list.push(column[year][number][2]);
+          four_list.push(column[year][number][3]);
+          five_list.push(column[year][number][4]);
+          six_list.push(column[year][number][5]);
+        }
+
+        console.log(one_list);
+        console.log(three_list);
+        console.log(six_list);
+
+        setDatasets([
+          {
+            label: "1week",
+            backgroundColor: "coral",
+            borderColor: "coral",
+            borderWidth: 1,
+            hoverBackgroundColor: "coral",
+            hoverBorderColor: "coral",
+            data: one_list,
+          },
+          {
+            label: "2week",
+            backgroundColor: "rgba(155,231,91,0.2)",
+            borderColor: "coral",
+            borderWidth: 1,
+            data: two_list,
+          },
+          {
+            label: "3week",
+            backgroundColor: "rgba(155,231,91,0.2)",
+            borderColor: "green",
+            borderWidth: 1,
+            data: three_list,
+          },
+          {
+            label: "4week",
+            backgroundColor: "rgba(155,231,91,0.2)",
+            borderColor: "black",
+            borderWidth: 1,
+            data: four_list,
+          },
+          {
+            label: "5week",
+            backgroundColor: "rgba(155,231,91,0.2)",
+            borderColor: "black",
+            borderWidth: 1,
+            data: five_list,
+          },
+          {
+            label: "6week",
+            backgroundColor: "rgba(155,231,91,0.2)",
+            borderColor: "black",
+            borderWidth: 1,
+            data: six_list,
+          },
+        ]);
+      }
+    }
+  }, [year]);
+
+  const YearChangeBtn = (year) => {
+    setYear(year);
+  };
 
   return (
     <>
@@ -52,7 +158,12 @@ const Statistics = () => {
         </SvgBorder>
 
         <ColumnBorder>
-          <ColumnChart data={column} />
+          <ColumnChart
+            labels={labels}
+            datasets={datasets}
+            year_list={year_list}
+            YearChangeBtn={YearChangeBtn}
+          />
         </ColumnBorder>
       </div>
     </>
