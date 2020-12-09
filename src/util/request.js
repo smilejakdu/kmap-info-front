53272c44e3fe7b31a74dc3a7dd1337588279e725
안승현 <kaipharm_shahn@anseunghyeon-ui-MacBookPro.local>
2020-12-10 02:16:26 +0900

    url_test2

--- src/util/request.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const request = axios.create({
-  // baseURL: 'https://apps.kaipharm.com/kmapinfo/api',
-  baseURL: 'http://localhost:8000/',
-});
-
-export default request;
